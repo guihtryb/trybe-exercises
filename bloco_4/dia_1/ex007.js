7386680cca5182e358dba5bc118c6a9ed3a1e6fb
Guilherme Viana <guilhermehviana01@gmail.com>
2021-05-19 19:19:49 -0300

    Adiciona lógica para retornar o maior entre 3 valores

---
+++ bloco_4/dia_1/ex007.js
@@ -0,0 +1,20 @@
+let a = 5;
+let b = 10;
+let c = 15;
+
+if (a>b && a>c) {
+    console.log(a)
+}
+
+else if(b>a && b>c) {
+    console.log(b)
+}
+
+else if(c>a && c>b) {
+    console.log(c)
+}
+
+else {
+    console.log('Os valores são iguais')
+}
+
