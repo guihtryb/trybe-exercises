168db7d738608245d86f5e5974e80f07622c213e
Guilherme Viana <guilhermehviana01@gmail.com>
2021-05-19 19:35:15 -0300

    Adiciona lógica para identificar se os angulos são pertencentes a um triângulo

---
+++ bloco_4/dia_1/ex009.js
@@ -0,0 +1,17 @@
+ang1 = 30;
+ang2 = 90;
+ang3 = 60;
+
+soma = ang1 + ang2 + ang3;
+
+switch (soma === 180) {
+
+    case true:
+    console.log('true');
+    break;
+
+    case false:
+    console.log('false');
+
+}
+    
