f49e848a3d3d484677417357d696bedc6db8a9f5
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-04 09:05:27 -0300

    Exercício 1 - operações aritméticas

---
+++ bloco_4_Introdução_a_JavaScript/introducao_a_javaScript_e_logica.1/exercises.js
@@ -0,0 +1,11 @@
+// 1. Operações aritméticas
+const a = 15;
+const b = 10;
+
+console.log(`Adição de ${a} com ${b} é igual a: ${a + b}`);
+console.log(`Subtração de ${a} com ${b} é igual a: ${a - b}`);
+console.log(`Multiplicação de ${a} com ${b} é igual a: ${a * b}`);
+console.log(`Divisão de ${a} com ${b} é igual a: ${a / b}`);
+console.log(`Módulo de ${a} com ${b} é igual a: ${a % b}`);
+
+
