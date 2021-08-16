fcc2a0fc04886aa8ed2d1b59cb820d16850761c7
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-16 07:38:19 -0300

    Exercise 1 - Go through all the array and log each one of his values

---
+++ Fundamentos Do Desenvolvimento Web/bloco_4/introducao_a_javascript_e_logica_de_programacao.2/exercises_day2.js
@@ -0,0 +1,7 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Exercise 1 - Go through all the array above and log each one of his values
+
+for (let index = 0; index < numbers.length; index+=1) {
+  console.log(numbers[index]);
+}
