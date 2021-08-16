3d3477e5e71111cd8a32e19dd7a470bd25cd73e6
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-16 07:45:04 -0300

    Exercise 2 - sum all the values of the array and log the final result

--- Fundamentos Do Desenvolvimento Web/bloco_4/introducao_a_javascript_e_logica_de_programacao.2/exercises_day2.js
+++ Fundamentos Do Desenvolvimento Web/bloco_4/introducao_a_javascript_e_logica_de_programacao.2/exercises_day2.js
@@ -5,3 +5,11 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index = 0; index < numbers.length; index+=1) {
   console.log(numbers[index]);
 }
+
+// Exercise 2 - sum all the values of the array an log the final result
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+for (let index = 0; index < numbers.length; index++) {
+  sum += numbers[index];
+}
+console.log(sum);
