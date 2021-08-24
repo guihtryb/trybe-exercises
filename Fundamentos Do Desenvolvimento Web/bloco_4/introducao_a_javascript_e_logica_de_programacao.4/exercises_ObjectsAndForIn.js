fa151c8a3c16a22ca4033ddb0a0f181929cd3256
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-24 20:56:50 -0300

    Exercise 1 - Log a welcome message for the character above

---
+++ Fundamentos Do Desenvolvimento Web/bloco_4/introducao_a_javascript_e_logica_de_programacao.4/exercises_ObjectsAndForIn.js
@@ -0,0 +1,11 @@
+let info = {
+  character: 'Margarida',
+  origin: 'Pato Donald',
+  note: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+// acessar oconsole passando o método log
+// concatenar string 'Bem-vinda, ' com o valor da chave margarida
+
+// Exercise 1 - Log a welcome message for the character above
+console.log('Bem-vinda, ' + info.personagem);
