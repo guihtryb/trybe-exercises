cc7c1c02824832bb8a3fe4eb4fe87cbd1e9b84ef
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-24 21:16:35 -0300

    Exercise 5 - Create a new object with the same keys of 'info' but with different values. Then log 'object1.value and object2.value' for each key

--- Fundamentos Do Desenvolvimento Web/bloco_4/introducao_a_javascript_e_logica_de_programacao.4/exercises_ObjectsAndForIn.js
+++ Fundamentos Do Desenvolvimento Web/bloco_4/introducao_a_javascript_e_logica_de_programacao.4/exercises_ObjectsAndForIn.js
@@ -1,4 +1,4 @@
-let info = {
+let info1 = {
   character: 'Margarida',
   origin: 'Pato Donald',
   note: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
@@ -10,22 +10,37 @@ let info = {
 // Acess console passing the 'log' method.
 // concatenate string 'Bem-vinda, ' with the value of the key 'character'
 
-console.log('Bem-vinda, ' + info.personagem);
+console.log('Bem-vinda, ' + info1.personagem);
 
 // Exercise 2 - Add a new property to the object with the key name 'recurrent' and the value 'Yes'.
 
-info['recurrent'] = 'Yes';
+info1['recurrent'] = 'Yes';
 
-console.log(info);
+console.log(info1);
 
 // Exercise 3 - Do a for/in that shows all the keys of the 'info' object
 
-for (let key in info) {
+for (let key in info1) {
   console.log(key);
 }
 
 // Exercise 4 - Do a for/in that shows all the values of the 'info' object keys
 
-for (let key in info) {
-  console.log(info[key]);
+for (let key in info1) {
+  console.log(info1[key]);
+}
+
+// Exercise 5 - Create a new object with the same keys of 'info' but with different values. Then log 'object1.value and object2.value' for each key
+
+let info2 = {
+  character: 'Tio Patinhas',
+  origin: `Christmas on Bear Mountain, Dell's Four Color Comics #178`,
+  note: 'O último MacPatinhas',
+  recurrent: 'Yes',
+};
+
+// if the values in 'recurrent' are equals, log 'Both recurring'
+for (let key in info1 ) {
+  if (info1[key] === info2[key]) console.log('Both recurring');
+  else console.log(info1[key] + ' and ' + info2[key]);
 }
