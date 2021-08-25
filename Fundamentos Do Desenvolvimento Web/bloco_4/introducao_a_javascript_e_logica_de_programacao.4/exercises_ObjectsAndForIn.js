a90df3df70b10083cab391be0188622f2dcd02f6
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-24 21:02:59 -0300

    Exercise 2 - Add a new property to the object with the key name 'recurrent' and the value 'Yes'

--- Fundamentos Do Desenvolvimento Web/bloco_4/introducao_a_javascript_e_logica_de_programacao.4/exercises_ObjectsAndForIn.js
+++ Fundamentos Do Desenvolvimento Web/bloco_4/introducao_a_javascript_e_logica_de_programacao.4/exercises_ObjectsAndForIn.js
@@ -4,8 +4,17 @@ let info = {
   note: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-// acessar oconsole passando o método log
-// concatenar string 'Bem-vinda, ' com o valor da chave margarida
 
 // Exercise 1 - Log a welcome message for the character above
+
+// Acess console passing the 'log' method.
+// concatenate string 'Bem-vinda, ' with the value of the key 'character'
+
 console.log('Bem-vinda, ' + info.personagem);
+
+// Exercise 2 - Add a new property to the object with the key name 'recurrent' and the value 'Yes'.
+
+info['recurrent'] = 'Yes';
+
+console.log(info);
+
