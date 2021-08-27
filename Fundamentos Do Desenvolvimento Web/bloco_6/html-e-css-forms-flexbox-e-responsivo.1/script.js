d8790b31ecaf010d55e0be23edfa0415f2f29731
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-27 17:46:13 -0300

    Create first fieldset for the forms and add addStatesToSelect function

---
+++ Fundamentos Do Desenvolvimento Web/bloco_6/html-e-css-forms-flexbox-e-responsivo.1/script.js
@@ -0,0 +1,54 @@
+const states = [
+  { 'AC': 'Acre' },
+  { 'AL': 'Alagoas' },
+  { 'AP': 'Amapá' },
+  { 'AM': 'Amazonas' },
+  { 'BA': 'Bahia' },
+  { 'CE': 'Ceará' },
+  { 'DF': 'Distrito Federal' },
+  { 'ES': 'Espírito Santo' },
+  { 'GO': 'Goías' },
+  { 'MA': 'Maranhão' },
+  { 'MT': 'Mato Grosso' },
+  { 'MS': 'Mato Grosso do Sul' },
+  { 'MG': 'Minas Gerais' },
+  { 'PA': 'Pará' },
+  { 'PB': 'Paraíba' },
+  { 'PR': 'Paraná' },
+  { 'PE': 'Pernambuco' },
+  { 'PI': 'Piauí' },
+  { 'RJ': 'Rio de Janeiro' },
+  { 'RN': 'Rio Grande do Norte' },
+  { 'RS': 'Rio Grande do Sul' },
+  { 'RO': 'Rondônia' },
+  { 'RR': 'Roraíma' },
+  { 'SC': 'Santa Catarina' },
+  { 'SP': 'São Paulo' },
+  { 'SE': 'Sergipe' },
+  { 'TO': 'Tocantins' },
+];
+
+window.onload = function() {
+console.log('it`s working!');
+
+function addStatesToSelect() {
+    const select = document.getElementById('states');
+    for(state of states) {
+      const option = document.createElement('option');
+      option.innerText = `${Object.keys(state)} -${Object.values(state)}`
+      select.appendChild(option);
+    }
+  }
+
+addStatesToSelect();
+}
+
+
+for(state of states) {
+  // const option = document.createElement('option');
+  // option.innerText = `${Object.keys(states[state])} - ${Object.values(states[state])}`
+  // select.appendChild(option);
+  console.log();
+}
+
+
