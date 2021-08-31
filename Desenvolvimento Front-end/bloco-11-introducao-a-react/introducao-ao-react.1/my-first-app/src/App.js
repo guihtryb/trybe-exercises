875b88ce44c1d468c14aa2c9815d131491566df0
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-31 05:57:43 -0300

    Creates an react app and init a task list to it

---
+++ Desenvolvimento Front-end/bloco-11-introducao-a-react/introducao-ao-react.1/my-first-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+function App() {
+  return (
+    <ul>
+      {Task("Buy bread")}
+    </ul>
+  );
+}
+
+export default App;
