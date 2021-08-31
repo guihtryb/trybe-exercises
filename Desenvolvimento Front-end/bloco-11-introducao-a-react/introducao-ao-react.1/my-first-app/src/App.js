ed534abf65eff55a1e1f15ad8eddccd04155e859
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-31 06:21:35 -0300

    Finish list using map method in React exercise

--- Desenvolvimento Front-end/bloco-11-introducao-a-react/introducao-ao-react.1/my-first-app/src/App.js
+++ Desenvolvimento Front-end/bloco-11-introducao-a-react/introducao-ao-react.1/my-first-app/src/App.js
@@ -1,13 +1,18 @@
+import './App.css'
+
 const Task = (value) => {
   return (
-    <li>{value}</li>
+    <li className="task-content">{value}</li>
   );
 }
+
+const appointments = ['Organize my GitHub', 'Refactor To-do list projec', 'Refactor pixel art project', 'Build a portfolio based in React'];
 
 function App() {
   return (
     <ul>
       {Task('Buy bread')}
+      {appointments.map((appointment) => (Task(appointment)))}
     </ul>
   );
 }
